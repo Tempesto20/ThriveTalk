1a23f0ba4c9166192d8922371e0eb3941e7250d7
Tempesto20 <Bro74rus1@mail.ru>
2021-05-17 20:23:14 +0300

    якорь

--- src/js/script.js
+++ src/js/script.js
@@ -9,4 +9,48 @@ window.onload = function () {
 		burger.classList.toggle('active');
 		nav.classList.toggle('active');
 	};
+
+	// Кнопка наверх
+	function trackScroll() {
+		var scrolled = window.pageYOffset;
+		var coords = document.documentElement.clientHeight;
+
+		if (scrolled > coords) {
+			goTopBtn.classList.add('back-to-top__show');
+		}
+		if (scrolled < coords) {
+			goTopBtn.classList.remove('back-to-top__show');
+		}
+	}
+
+	function backToTop() {
+		if (window.pageYOffset > 0) {
+			window.scrollBy(0, -80);
+			setTimeout(backToTop, 0);
+		}
+	}
+
+	var goTopBtn = document.querySelector('.back-to-top');
+
+	window.addEventListener('scroll', trackScroll);
+	goTopBtn.addEventListener('click', backToTop);
+
+	// ---------------
+
+	// Плавные якоря
+	const anchors = document.querySelectorAll('a[href*="#"]')
+
+	for (let anchor of anchors) {
+		anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+			
+			const blockID = anchor.getAttribute('href').substr(1);
+
+			document.getElementById(blockID).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			})
+		})
+	}
+	// ---------------
 };
